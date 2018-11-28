6a3ec4ea8914b4dea7afd485d423dfb2bb282c42
Rebecca <rebecca@psafe.com>
2018-11-28 18:04:05 -0200

    cover

--- src/component/dashboard/Cover.js
+++ src/component/dashboard/Cover.js
@@ -1,16 +1,62 @@
 import React, { Component } from 'react'
+import moment from 'moment'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { Link } from 'react-router-dom'
+import { Row, Col } from 'react-bootstrap'
 
 // COMPONENT IMPORT
 import Brand from '../layout/Brand'
 
 export class Cover extends Component {
   render() {
+
+    const { posts } = this.props
+
     return (
       <div>
           <Brand brand='cover'/>
+
+          <div className='content'>         
+                
+              <Row className='container'>
+
+                    {
+                      posts && posts.slice(0,4).map(post => {
+                        return (
+                          <Col md={3}>
+                              <div className='recent-post'>
+                                  <label className='image'>
+  
+                                      <img src={post.image} alt='' />
+  
+                                  </label>
+  
+                                  <label className='title'>{post.title}</label>
+                                  <label className='authorAndDate'>{post.author} * {moment(post.createdAt.toDate()).calendar()}}</label>
+  
+                                  <span className='readBtn' ><Link to={`/posts/${post.id}`}>Read More</Link></span>
+                              </div>      
+                          </Col>
+                        )
+                      })   
+                    } 
+
+              </Row>
+          
+          </div>
       </div>
     )
   }
 }
 
-export default Cover
+const mapStateToProps = (state) => {
+  return {
+      posts: state.firestore.ordered.posts
+  }
+}
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: 'posts' }])   
+)(Cover)
--- src/style/style.js
+++ src/style/style.js
@@ -1,7 +1,9 @@
 import './index.css'
+import './cover.css'
 import './navbar.css'
 import './footer.css'
 import './brand.css'
+import './colors.css'
 
 import './createPost.css'
 import './postSummary.css'

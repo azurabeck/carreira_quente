b165951ee13d5ea8b1fd7b7e279bc2214b7fceea
Rebecca <rebecca@psafe.com>
2018-12-06 12:41:01 -0200

    layout changes on summary

--- src/component/posts/PostSummary.js
+++ src/component/posts/PostSummary.js
@@ -19,13 +19,17 @@ const PostSummary = (props) => {
                 </Col>
 
                 <Col md={8}>
-                    <label className='postTitle'>{post.title}</label>          
-                    <label className='postDescription'>{renderHTML(post.content)} </label>
+                    <label className='postTitle'>{post.title}</label>   
+
                     <ul className='postDetails'>
                         <li>Author: <span>{post.authorLastName} {post.authorFirstName}</span></li>
                         <li>Post at: <span>{moment(post.createdAt.toDate()).calendar()}</span></li>
                     </ul>
+
+                    <label className='postDescription'>{renderHTML(post.content)} </label>                   
                 </Col>
+
+                <Col md={12} className='right readMore'>Continuar lendo...</Col>
             </Row>
         
         </div>
